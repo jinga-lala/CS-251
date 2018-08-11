273bfbf43878a32a97e7c383f8223dfc28b6d304
Yash Jain <yash.jain3599@gmail.com>
2018-08-11 15:58:26 +0530

    Delete parse.py
    galti se mistake

--- parse.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('infinity_stones.json') as f:
-    data = json.load(f)
-    w=open("infinity_stones.csv","w")
-    for stones in data["Infinity Stones"]:
-    	w.write(stones["Stone Name"]+","+stones["Stone Color"]+","+stones["Containment Unit"]+"\n")

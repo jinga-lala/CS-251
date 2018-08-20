7e808d542ed2e29effbca7a8ef78bd8d60a6ae38
Yash Jain <yash.jain3599@gmail.com>
2018-08-21 00:34:53 +0530

    Initial commit

---
+++ outlab5/P1/build/CMakeFiles/libMyEngineStatic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libMyEngineStatic.dir/myengine/myengine.cpp.o"
+  "liblibMyEngineStatic.pdb"
+  "liblibMyEngineStatic.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libMyEngineStatic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
